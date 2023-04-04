438e548e0311126fce90f51ab18abd054306f3d4
Joao Vitor <100004933+Jao1br@users.noreply.github.com>
2023-04-04 13:12:16 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var lancer = 550
+var turbo = 143
+var pneus = 134
+var nitro = 430
+var carro = 5000
+
+function carro(){
+    lancer + turbo + pneus + nitro
+    
+}
+
+console.log("carro")
+   
+
+var nome = window.prompt("Qual Seu carro?")
+window.alert('Registre seu ' + nome + ' no nosso site e receba frete grátis na compra de peças')
